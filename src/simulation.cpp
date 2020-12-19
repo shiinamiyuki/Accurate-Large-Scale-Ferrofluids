edfb2359215aa6dafc02e64145ebbd93ef57ff05
drixs2050 <39656972+drixs2050@users.noreply.github.com>
2020-12-19 13:40:56 -0500

    formatted and removed ref to float

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,7 +1,7 @@
 #include "simulation.h"
-#include <tbb/parallel_for.h>
 #include <Eigen/Core>
 #include <Eigen/Sparse>
+#include <tbb/parallel_for.h>
 constexpr double pi = 3.1415926535897;
 // https://github.com/erizmr/SPH_Taichi
 static inline double W(double r, double h) {
@@ -225,25 +225,23 @@ void Simulation::run_step_euler() {
     // printf("step done\n");
 }
 
-vec3 Simulation::H(vec3 r, vec3 m){
+vec3 Simulation::H(vec3 r, vec3 m) {
     float r_norm = dot(r, r);
     vec3 r_hat = normalize(r);
-    vec3 H_r = dot(r_hat, m) * (W_avr(r) - W(r)) * r_hat - (W_avr(r)/3.0f) * m;
+    vec3 H_r = dot(r_hat, m) * (W_avr(r) - W(r)) * r_hat - (W_avr(r) / 3.0f) * m;
     return H_r;
 }
 
-float Simulation::W_avr(vec3 r) { 
+float Simulation::W_avr(vec3 r) {
     float r_norm = dot(r, r);
     float W_r_h = 0.0;
-    float q = r_norm/h;
-    if (0 <= q && q < 1){
+    float q = r_norm / h;
+    if (0 <= q && q < 1) {
         W_r_h = (1.0 / 40.0) * (15.0 * pow(q, 3) - 36 * pow(q, 2) + 40.0);
-    }
-    else if (1 <= q && q < 2){
-        W_r_h = (-3.0/(4.0 * pow(q, 3))) * (pow(q, 6)/6.0 - (6.0 * pow(q, 5))/5.0 + 
-                3.0 * pow(q, 4) - (8.0 * pow(q, 3))/3.0 + 1.0/15.0);
-    }
-    else{
+    } else if (1 <= q && q < 2) {
+        W_r_h = (-3.0 / (4.0 * pow(q, 3))) *
+                (pow(q, 6) / 6.0 - (6.0 * pow(q, 5)) / 5.0 + 3.0 * pow(q, 4) - (8.0 * pow(q, 3)) / 3.0 + 1.0 / 15.0);
+    } else {
         W_r_h = 3.0 / (4.0 * pow(q, 3));
     }
     W_r_h *= (1.0 / pi);
@@ -254,11 +252,11 @@ float Simulation::W_avr(vec3 r) {
 float Simulation::W(vec3 r) {
     float r_norm = dot(r, r);
     float W_r_h = 0.0;
-    float q = r_norm/h;
-    if (0 <= q && q < 1){
-        W_r_h = 0.25 * pow((2.0 - q), 3) - pow((1.0 - q), 3); 
+    float q = r_norm / h;
+    if (0 <= q && q < 1) {
+        W_r_h = 0.25 * pow((2.0 - q), 3) - pow((1.0 - q), 3);
     }
-    if (1 <= q && q < 2){
+    if (1 <= q && q < 2) {
         W_r_h = 0.25 * pow((2.0 - q), 3);
     }
     W_r_h *= (1.0 / pi);
@@ -269,11 +267,11 @@ float Simulation::W(vec3 r) {
 float Simulation::dWdr(vec3 r) {
     float r_norm = dot(r, r);
     float dW_r_h = 0.0;
-    float q = r_norm/h;
-    if (0 <= q && q < 1){
+    float q = r_norm / h;
+    if (0 <= q && q < 1) {
         dW_r_h = 2.25 * pow(q, 2) - 3.0 * q;
     }
-    if (1 <= q && q < 2){
+    if (1 <= q && q < 2) {
         dW_r_h = -0.75 * pow(q, 2) + 3.0 * q - 3.0;
     }
     dW_r_h *= (1.0 / pi);
@@ -286,7 +284,7 @@ void Simulation::eval_Hext() {
     // single point magnetic field
 }
 
-void Simulation::get_R(Eigen::Matrix3d &R, const Eigen::Vector3d &rt, const Eigen::Vector3d &rs){
+void Simulation::get_R(Eigen::Matrix3d &R, const Eigen::Vector3d &rt, const Eigen::Vector3d &rs) {
     // given rt and rs world coordinate, transform it to the coordinate where rs is on origin
     // let (xi, eta, zeta) be the unit vectors and assume rt is on its zeta axis.
     Eigen::Vector3d zeta = (rt - rs).normalized();
@@ -303,84 +301,98 @@ void Simulation::get_R(Eigen::Matrix3d &R, const Eigen::Vector3d &rt, const Eige
 }
 
 // data provided in appendix
-float Simulation::get_C1(float &q){
+float Simulation::get_C1(float q) {
     float C1;
-    if (0 < q&&q <= 1){
-        C1 = q*(q*(q*(q*(9.97813616438174e-09)+(-2.97897856524718e-08))+(2.38918644566813e-09))+(4.53199938857366e-08))+(2.44617454752747e-11);
-    }
-    else if (1 < q&&q <= 2){
-        C1 = q*(q*(q*(q*(-2.76473728643294e-09)+(2.86975546540539e-08))+(-9.94582836806651e-08))+(1.25129924573675e-07))+(-2.37010166723652e-08);
-    }
-    else if (2 < q&&q <= 3){
-        C1 = q*(q*(q*(q*(-1.09679990621465e-09)+(9.77055663264614e-09))+(-2.54781238661150e-08))+(2.65020634884934e-09))+(5.00787562417835e-08);
-    }
-    else if (3 < q&&q <= 4){
-        C1 = q*(q*(q*(q*(3.79927162333632e-10)+(-6.26368404962679e-09))+(3.94760528277489e-08))+(-1.13580541622200e-07))+(1.27491333574323e-07);
+    if (0 < q && q <= 1) {
+        C1 = q * (q * (q * (q * (9.97813616438174e-09) + (-2.97897856524718e-08)) + (2.38918644566813e-09)) +
+                  (4.53199938857366e-08)) +
+             (2.44617454752747e-11);
+    } else if (1 < q && q <= 2) {
+        C1 = q * (q * (q * (q * (-2.76473728643294e-09) + (2.86975546540539e-08)) + (-9.94582836806651e-08)) +
+                  (1.25129924573675e-07)) +
+             (-2.37010166723652e-08);
+    } else if (2 < q && q <= 3) {
+        C1 = q * (q * (q * (q * (-1.09679990621465e-09) + (9.77055663264614e-09)) + (-2.54781238661150e-08)) +
+                  (2.65020634884934e-09)) +
+             (5.00787562417835e-08);
+    } else if (3 < q && q <= 4) {
+        C1 = q * (q * (q * (q * (3.79927162333632e-10) + (-6.26368404962679e-09)) + (3.94760528277489e-08)) +
+                  (-1.13580541622200e-07)) +
+             (1.27491333574323e-07);
     }
     return C1;
 }
 
-float Simulation::get_C2(float &q){
+float Simulation::get_C2(float q) {
     float C2;
-    if (0 < q&&q <= 1){
-        C2 = q*(q*(q*(q*(6.69550479838731e-08)+(-1.61753307173877e-07))+(1.68213714992711e-08))+(1.34558143036838e-07))+(1.10976027980100e-10);
-    }
-    else if (1 < q&&q <= 2){
-        C2 = q*(q*(q*(q*(-3.08460139955194e-08)+(2.29192245602275e-07))+(-5.88399621128587e-07))+(5.61170054591844e-07))+(-1.14421132829680e-07);
-    }
-    else if (2 < q&&q <= 3){
-        C2 = q*(q*(q*(q*(3.50477408060213e-09)+(-5.25956271895141e-08))+(2.78876509535747e-07))+(-6.24199554212217e-07))+(4.91807818904985e-07);
-    }
-    else if (3 < q&&q <= 4){
-        C2 = q*(q*(q*(q*(7.33485346367840e-10)+(-9.58855788627803e-09))+(4.37085309763591e-08))+(-7.48004594092261e-08))+(2.34161209605651e-08);
+    if (0 < q && q <= 1) {
+        C2 = q * (q * (q * (q * (6.69550479838731e-08) + (-1.61753307173877e-07)) + (1.68213714992711e-08)) +
+                  (1.34558143036838e-07)) +
+             (1.10976027980100e-10);
+    } else if (1 < q && q <= 2) {
+        C2 = q * (q * (q * (q * (-3.08460139955194e-08) + (2.29192245602275e-07)) + (-5.88399621128587e-07)) +
+                  (5.61170054591844e-07)) +
+             (-1.14421132829680e-07);
+    } else if (2 < q && q <= 3) {
+        C2 = q * (q * (q * (q * (3.50477408060213e-09) + (-5.25956271895141e-08)) + (2.78876509535747e-07)) +
+                  (-6.24199554212217e-07)) +
+             (4.91807818904985e-07);
+    } else if (3 < q && q <= 4) {
+        C2 = q * (q * (q * (q * (7.33485346367840e-10) + (-9.58855788627803e-09)) + (4.37085309763591e-08)) +
+                  (-7.48004594092261e-08)) +
+             (2.34161209605651e-08);
     }
     return C2;
 }
 
-void Simulation::get_T_hat(Eigen::Matrix3d &T_hat, const Eigen::Vector3d &m_hat_s, float & q){
+void Simulation::get_T_hat(Eigen::Matrix3d &T_hat, const Eigen::Vector3d &m_hat_s, float q) {
     float C1 = get_C1(q);
     float C2 = get_C2(q);
-    T_hat << m_hat_s[2] * C1, 0, m_hat_s[0] * C1,
-             0, m_hat_s[2] * C1, m_hat_s[1] * C1,
-             m_hat_s[0] * C1, m_hat_s[1] * C1, m_hat_s[2] * C2;
+    T_hat << m_hat_s[2] * C1, 0, m_hat_s[0] * C1, 0, m_hat_s[2] * C1, m_hat_s[1] * C1, m_hat_s[0] * C1, m_hat_s[1] * C1,
+        m_hat_s[2] * C2;
 }
 
-void Simulation::get_Force_Tensor(Eigen::Matrix3d &Ts, const Eigen::Vector3d &rt, const Eigen::Vector3d &rs, const Eigen::Vector3d &ms){
+void Simulation::get_Force_Tensor(Eigen::Matrix3d &Ts, const Eigen::Vector3d &rt, const Eigen::Vector3d &rs,
+                                  const Eigen::Vector3d &ms) {
     Eigen::Vector3d r = rt - rs;
     vec3 r_for_W = vec3(r[0], r[1], r[2]);
     float r_norm = dot(r_for_W, r_for_W);
-    float Ar = (W_avr(r_for_W) - W(r_for_W))/pow(r_norm, 2);
-    float Ar_prime = (5 * W(r_for_W))/pow(r_norm, 3) - (5 * W_avr(r_for_W))/pow(r_norm, 3) - dWdr(r_for_W)/pow(r_norm, 2);
+    float Ar = (W_avr(r_for_W) - W(r_for_W)) / pow(r_norm, 2);
+    float Ar_prime =
+        (5 * W(r_for_W)) / pow(r_norm, 3) - (5 * W_avr(r_for_W)) / pow(r_norm, 3) - dWdr(r_for_W) / pow(r_norm, 2);
     Ts = (Eigen::Matrix3d::Identity() * (r.transpose() * ms) + r * ms.transpose() + ms * r.transpose()) * Ar +
-            r * (r.transpose() * ms) *(r.transpose()/r_norm) * Ar_prime;
+         r * (r.transpose() * ms) * (r.transpose() / r_norm) * Ar_prime;
 }
 
-void Simulation::compute_m(const Eigen::VectorXd &b){
+void Simulation::compute_m(const Eigen::VectorXd &b) {
     Eigen::VectorXd Gamma_b = Gamma * b;
-    for(size_t i = 0; i < num_particles; i++){
-        pointers.particle_mag_moment[i] = vec3(Gamma_b.segment<3>(i * 3)[0],Gamma_b.segment<3>(i * 3)[1], Gamma_b.segment<3>(i * 3)[2]);
+    for (size_t i = 0; i < num_particles; i++) {
+        pointers.particle_mag_moment[i] =
+            vec3(Gamma_b.segment<3>(i * 3)[0], Gamma_b.segment<3>(i * 3)[1], Gamma_b.segment<3>(i * 3)[2]);
     }
 }
 void Simulation::magnetization() {
-    for(size_t t = 0; t < num_particles; t++){
+    for (size_t t = 0; t < num_particles; t++) {
         pointers.particle_H[t] = vec3(0.0f, 0.0f, 0.0f);
         pointers.particle_M[t] = vec3(0.0f, 0.0f, 0.0f);
-        for(size_t s = 0; s < num_particles; s++){
-            pointers.particle_H[t] += H(pointers.particle_position[t] - pointers.particle_position[s], pointers.particle_mag_moment[s]);
-            pointers.particle_M[t] += pointers.particle_mag_moment[s] * W(pointers.particle_position[t] - pointers.particle_position[s]);
+        for (size_t s = 0; s < num_particles; s++) {
+            pointers.particle_H[t] +=
+                H(pointers.particle_position[t] - pointers.particle_position[s], pointers.particle_mag_moment[s]);
+            pointers.particle_M[t] +=
+                pointers.particle_mag_moment[s] * W(pointers.particle_position[t] - pointers.particle_position[s]);
         }
     }
 }
 
 void Simulation::compute_magenetic_force() {
     Eigen::VectorXd hext(3 * num_particles), b;
-    for(size_t i = 0; i < num_particles; i++){
+    for (size_t i = 0; i < num_particles; i++) {
         hext << pointers.Hext[i][0], pointers.Hext[i][1], pointers.Hext[i][2];
     }
     Eigen::SparseMatrix<double> G;
     G.resize(3 * num_particles, 3 * num_particles);
     std::vector<Eigen::Triplet<double>> trip;
-    for(size_t j = 0; j < num_particles; j++){
+    for (size_t j = 0; j < num_particles; j++) {
         trip.push_back(Eigen::Triplet<double>(3 * j, 3 * j, pointers.particle_position[j][0]));
         trip.push_back(Eigen::Triplet<double>(3 * j + 1, 3 * j + 1, pointers.particle_position[j][1]));
         trip.push_back(Eigen::Triplet<double>(3 * j + 2, 3 * j + 2, pointers.particle_position[j][2]));
@@ -389,7 +401,7 @@ void Simulation::compute_magenetic_force() {
     Eigen::SparseMatrix<double> ident;
     ident.setIdentity();
     Eigen::SparseMatrix<double> A = G * Gamma - ident;
-    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>>cg;
+    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>> cg;
     cg.compute(A);
     b = cg.solve(-1.0 * hext);
     b = cg.solve(-1.0 * hext);
@@ -398,25 +410,26 @@ void Simulation::compute_magenetic_force() {
     Eigen::Matrix3d R, Ts, T_hat;
     float q;
     double dist;
-    for(size_t t = 0; t < num_particles; t++){
+    for (size_t t = 0; t < num_particles; t++) {
         Eigen::Matrix3d U;
         U.setZero();
         Eigen::Vector3d rt, mt;
         rt << pointers.particle_position[t][0], pointers.particle_position[t][1], pointers.particle_position[t][2];
-        mt << pointers.particle_mag_moment[t][0], pointers.particle_mag_moment[t][1], pointers.particle_mag_moment[t][2];
-        for(size_t s = 0; s < num_particles; s++){
+        mt << pointers.particle_mag_moment[t][0], pointers.particle_mag_moment[t][1],
+            pointers.particle_mag_moment[t][2];
+        for (size_t s = 0; s < num_particles; s++) {
             Eigen::Vector3d rs, ms;
             rs << pointers.particle_position[s][0], pointers.particle_position[s][1], pointers.particle_position[s][2];
-            ms << pointers.particle_mag_moment[s][0], pointers.particle_mag_moment[s][1], pointers.particle_mag_moment[s][2];
-            dist = (rt-rs).norm();
-            if ( dist < 4.0 * h){
-                q = dist/h;
-                get_R(R, rt, rs); //note rs is the source!
+            ms << pointers.particle_mag_moment[s][0], pointers.particle_mag_moment[s][1],
+                pointers.particle_mag_moment[s][2];
+            dist = (rt - rs).norm();
+            if (dist < 4.0 * h) {
+                q = dist / h;
+                get_R(R, rt, rs); // note rs is the source!
                 m_hat = R.transpose() * ms;
                 get_T_hat(T_hat, m_hat, q);
                 Ts = R * T_hat * R.transpose();
-            }
-            else{
+            } else {
                 get_Force_Tensor(Ts, rt, rs, ms);
             }
             U += Ts;
